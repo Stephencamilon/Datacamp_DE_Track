8216ac2759856601d7efdb3484188d5d455cfa9b
Stephen Camilon <113228328+Stephencamilon@users.noreply.github.com>
2024-01-08 01:51:48 +0800

    Create 2.1_Inconsistent_categories.py

---
+++ 12_Cleaning_Data_in_Python/2_Text_and_Categorical_data_problems/2.1_Inconsistent_categories.py
@@ -0,0 +1,10 @@
+'''
+Instructions:
+
+1. Print the unique values in dest_region and dest_size respectively.
+
+'''
+
+# Print unique values of both columns
+print(airlines['dest_region'].unique())
+print(airlines['dest_size'].unique())
