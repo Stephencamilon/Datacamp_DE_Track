5a7a4bf92f950edf69349b4ed722d7036ba8bfe2
Stephen Camilon <113228328+Stephencamilon@users.noreply.github.com>
2024-01-08 02:13:02 +0800

    Create 4_Removing_titles_and_taking_names.py

---
+++ 12_Cleaning_Data_in_Python/2_Text_and_Categorical_data_problems/4_Removing_titles_and_taking_names.py
@@ -0,0 +1,22 @@
+'''
+
+Instructions:
+1. Remove "Dr.", "Mr.", "Miss" and "Ms." from full_name by replacing them with an empty string "" in that order.
+2. Run the assert statement using .str.contains() that tests whether full_name still contains any of the honorifics.
+
+'''
+
+# Replace "Dr." with empty string ""
+airlines['full_name'] = airlines['full_name'].str.replace("Dr.","")
+
+# Replace "Mr." with empty string ""
+airlines['full_name'] = airlines['full_name'].str.replace("Mr.","")
+
+# Replace "Miss" with empty string ""
+airlines['full_name'] = airlines['full_name'].str.replace("Miss","")
+
+# Replace "Ms." with empty string ""
+airlines['full_name'] = airlines['full_name'].str.replace("Ms.","")
+
+# Assert that full_name has no honorifics
+assert airlines['full_name'].str.contains('Ms.|Mr.|Miss|Dr.').any() == False
