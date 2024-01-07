61a86c1836cdccbdb7b13282c0014b029805f132
Stephen Camilon <113228328+Stephencamilon@users.noreply.github.com>
2024-01-08 01:58:56 +0800

    Create 2.3_Inconsistent_categories.py

---
+++ 12_Cleaning_Data_in_Python/2_Text_and_Categorical_data_problems/2.3_Inconsistent_categories.py
@@ -0,0 +1,22 @@
+'''
+Instructions:
+
+1. Strip white spaces from the dest_size column using the .strip() method.
+2. Verify that the changes have been into effect by printing the unique values of the columns using .unique()
+
+'''
+
+# Print unique values of both columns
+print(airlines['dest_region'].unique())
+print(airlines['dest_size'].unique())
+
+# Lower dest_region column and then replace "eur" with "europe"
+airlines['dest_region'] = airlines['dest_region'].str.lower() 
+airlines['dest_region'] = airlines['dest_region'].replace({'eur':'europe'})
+
+# Remove white spaces from `dest_size`
+airlines['dest_size'] = airlines['dest_size'].str.strip()
+
+# Verify changes have been effected
+print(airlines['dest_region'].unique())
+print(airlines['dest_size'].unique())
