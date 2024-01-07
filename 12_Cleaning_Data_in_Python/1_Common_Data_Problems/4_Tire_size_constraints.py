fc2764f9fee8e57b2ea3e5d37746121303c89240
Stephen Camilon <113228328+Stephencamilon@users.noreply.github.com>
2024-01-07 22:58:58 +0800

    Update 4_Tire_size_constraints.py

--- 12_Cleaning_Data_in_Python/1_Common_Data_Problems/4_Tire_size_constraints.py
+++ 12_Cleaning_Data_in_Python/1_Common_Data_Problems/4_Tire_size_constraints.py
@@ -1,3 +1,12 @@
+"""
+Instructins:
+
+1. Convert ride_date to a datetime object using to_datetime(), then convert the datetime object into a date and store it in ride_dt column.
+2. Create the variable today, which stores today's date by using the dt.date.today() function.
+3. For all instances of ride_dt in the future, set them to today's date.
+4. Print the maximum date in the ride_dt column.
+"""
+
 # Convert tire_sizes to integer
 ride_sharing['tire_sizes'] = ride_sharing['tire_sizes'].astype('int')
 
