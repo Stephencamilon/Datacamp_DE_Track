f0e628286f44395807aed4cadacb036461a71cfd
Stephen Camilon <113228328+Stephencamilon@users.noreply.github.com>
2024-01-07 23:00:04 +0800

    Update 3_Summing_strings_and_concatenating_numbers.py

--- 12_Cleaning_Data_in_Python/1_Common_Data_Problems/3_Summing_strings_and_concatenating_numbers.py
+++ 12_Cleaning_Data_in_Python/1_Common_Data_Problems/3_Summing_strings_and_concatenating_numbers.py
@@ -1,3 +1,14 @@
+"""
+Instructions:
+
+1. Use the .strip() method to strip duration of "minutes" and store it in the duration_trim column.
+2. Convert duration_trim to int and store it in the duration_time column.
+3. Write an assert statement that checks if duration_time's data type is now an int.
+4. Print the average ride duration.
+
+"""
+
+
 # Strip duration of minutes
 ride_sharing['duration_trim'] = ride_sharing['duration'].str.strip('minutes')
 
