26b18f159a7b2a9d466306550666a9cb6f105f57
Stephen Camilon <113228328+Stephencamilon@users.noreply.github.com>
2024-01-08 01:07:32 +0800

    Create 1.1_Finding_Consistency.py

---
+++ 12_Cleaning_Data_in_Python/2_Text_and_Categorical_data_problems/1.1_Finding_Consistency.py
@@ -0,0 +1,12 @@
+'''
+Print the categories DataFrame and take a close look at all possible correct categories of the survey columns.
+Print the unique values of the survey columns in airlines using the .unique() method.
+'''
+
+# Print categories DataFrame
+print(categories)
+
+# Print unique values of survey columns in airlines
+print('Cleanliness: ', airlines['cleanliness'].unique(), "\n")
+print('Safety: ', airlines['safety'].unique(), "\n")
+print('Satisfaction: ', airlines['satisfaction'].unique(), "\n")
