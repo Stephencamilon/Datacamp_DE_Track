ae9e0b7c56c6110a4bd60898f7c43af05d20e291
Stephen Camilon <113228328+Stephencamilon@users.noreply.github.com>
2024-01-09 03:35:08 +0800

    Create 4.3_Missing_investors.py

---
+++ 12_Cleaning_Data_in_Python/3_Advanced_data_problems/4.3_Missing_investors.py
@@ -0,0 +1,22 @@
+'''
+Instructions:
+
+Sort the banking DataFrame by the age column and plot the missingness matrix of banking_sorted.
+
+'''
+
+# Print number of missing values in banking
+print(banking.isna().sum())
+
+# Visualize missingness matrix
+msno.matrix(banking)
+plt.show()
+
+# Isolate missing and non missing values of inv_amount
+missing_investors = banking[banking['inv_amount'].isna()]
+investors = banking[~banking['inv_amount'].isna()]
+
+# Sort banking by age and visualize
+banking_sorted = banking.sort_values(by = 'age')
+msno.matrix(banking_sorted)
+plt.show()
