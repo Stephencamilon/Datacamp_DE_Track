180e43fae1e3efd017a48196b3457502d5ca52d6
Stephen Camilon <113228328+Stephencamilon@users.noreply.github.com>
2024-01-09 02:08:50 +0800

    Create 2.1_Uniform_dates.py

---
+++ 12_Cleaning_Data_in_Python/3_Advanced_data_problems/2.1_Uniform_dates.py
@@ -0,0 +1,9 @@
+'''
+Instructions:
+
+Print the header of account_opened from the banking DataFrame and take a look at the different results.
+
+'''
+
+# Print the header of account_opened
+print(banking['account_opened'].head())
