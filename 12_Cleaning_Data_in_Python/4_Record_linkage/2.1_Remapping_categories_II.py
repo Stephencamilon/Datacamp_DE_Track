782d51b71c6cfe5ed8e9b4d16ec149682459cc18
Stephen Camilon <113228328+Stephencamilon@users.noreply.github.com>
2024-01-09 16:44:19 +0800

    Create 2.1_Remapping_categories_II.py

---
+++ 12_Cleaning_Data_in_Python/4_Record_linkage/2.1_Remapping_categories_II.py
@@ -0,0 +1,10 @@
+'''
+
+Instructions:
+
+Return all of the unique values in the cuisine_type column of restaurants.
+
+'''
+
+# Inspect the unique values of the cuisine_type column
+print(restaurants['cuisine_type'].unique())
